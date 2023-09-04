6d415ef5d49916d3da8bbbbd7fa810f1a005f81e
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-09-04 23:55:54 +0300

    update rust_iteration program

--- rust_syntax/rust_iteration/src/main.rs
+++ rust_syntax/rust_iteration/src/main.rs
@@ -19,7 +19,34 @@ fn say_hello_simple(name: &str) {
 }
 
 
+fn say_hello_complex(name: &str) {
+
+    let mut characters = name.chars().peekable();
+
+    if let Some(&ch) = characters.peek() {
+
+        match ch {
+
+            'W' => {
+
+                println!("Hello, {}", name);
+            },
+
+            _ => {
+
+                println!("{}", name);
+            }
+        }
+    }
+
+}
+
+
 fn main() {
     say_hello_simple("Winfred");
     say_hello_simple("Mikey");
+
+
+    say_hello_complex("Wangechi");
+    say_hello_complex("Alice");
 }

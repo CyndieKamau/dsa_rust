a7cdc60dbb35f192fb02ac82a04d79c488bf2902
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-09-14 23:37:10 +0300

    bubble sort algorithm

---
+++ bubble_sort_algorithm/src/main.rs
@@ -0,0 +1,24 @@
+//Bubble Sort Algorithm
+//Big O Notation of O(n^2)
+
+//1. Start with the first element in the array
+//2. If the first item is greater than the second, swap them
+//3. Move to the next item (3rd item), repeat step 2 till end of array
+//N.B. You'll notice automatically the largest item in array will be sorted "bubbled up" at the end of the array
+//4. Repeat the same steps, excluding the last element as its already sorted
+//5. Continue iterating while excluding last item, till the array is sorted
+
+
+fn bubble_sort(array: &mut[i32]) {
+
+    
+
+
+}  
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}

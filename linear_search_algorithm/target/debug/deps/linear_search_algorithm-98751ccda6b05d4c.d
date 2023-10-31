0420c2de0444f56140a27cbcd42490fba5da7afb
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-10-31 04:11:32 +0300

    remove target folder linear search algorithm

--- linear_search_algorithm/target/debug/deps/linear_search_algorithm-98751ccda6b05d4c.d
+++
@@ -1,5 +0,0 @@
-/home/hp/Desktop/dsa_rust/linear_search_algorithm/target/debug/deps/linear_search_algorithm-98751ccda6b05d4c: src/main.rs
-
-/home/hp/Desktop/dsa_rust/linear_search_algorithm/target/debug/deps/linear_search_algorithm-98751ccda6b05d4c.d: src/main.rs
-
-src/main.rs:
--- linear_search_algorithm/target/debug/linear_search_algorithm.d
+++
@@ -1,1 +0,0 @@
-/home/hp/Desktop/dsa_rust/linear_search_algorithm/target/debug/linear_search_algorithm: /home/hp/Desktop/dsa_rust/linear_search_algorithm/src/main.rs
